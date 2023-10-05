49679975615a0c1fed9c70730403dc391ce94c4f
Andrea Lopez Palomeque <andrealopezpalomeque@gmail.com>
2023-10-04 21:29:11 -0300

    showing temperature city and icon on screen

--- Weather App/Controller/WeatherViewController.swift
+++ Weather App/Controller/WeatherViewController.swift
@@ -62,7 +62,16 @@ class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManag
     }
     
     func didUpdateWeather(weather: WeatherModel) {
-        print(weather.temperature)
+        DispatchQueue.main.async{
+            self.temperatureLabel.text = weather.temperatureString
+            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+            self.cityLabel.text = weather.cityName
+        }
+      
+    }
+    
+    func didFailWithError(error: Error) {
+        print(error)
     }
 }
 
--- Weather App/Model/WeatherManager.swift
+++ Weather App/Model/WeatherManager.swift
@@ -9,6 +9,7 @@ import Foundation
 
 protocol WeatherManagerDelegate {
     func didUpdateWeather(weather: WeatherModel)
+    func didFailWithError(error: Error)
 }
 
 struct WeatherManager {
@@ -32,7 +33,7 @@ struct WeatherManager {
             //closure format -> anonymous function
             let task = session.dataTask(with: url) { data, response, error in
                 if error != nil {
-                    print(error!)
+                    delegate?.didFailWithError(error: error!)
                     return
                 }
                 if let safeData = data {
@@ -60,9 +61,8 @@ struct WeatherManager {
             let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
             return weather
             
-           
         }  catch{
-            print(error)
+            delegate?.didFailWithError(error: error)
             return nil
         }
     }
